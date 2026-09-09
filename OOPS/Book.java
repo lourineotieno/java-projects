55c740b9594622eba9f2aa8cbf0e4a2b851f22ca
Lourine Otieno <lourineachieng55@gmail.com>
2026-09-09 22:20:55 +0300

    update book andadd vehicle

--- OOPS/Book.java
+++ OOPS/Book.java
@@ -1,15 +1,38 @@
 public class Book {
-    String tittle;
-    String author;
-    double price;
+    private String tittle;
+    private String author;
+    private double price;
     public Book(String tittle,String author,double price){
         this.tittle=tittle;
         this.author=author;
         this.price=price;
+    }
+    public String getTittle(){
+        return tittle;
     } 
+    public String getAuthor(){
+        return author;
+
+    }
+    public double  getPrice(){
+        return price;
+    }
+    public void setTittle(String tittle){
+        this.tittle=tittle;
+    }
+    public void setAuthor(String author){
+        this.author=author;
+    }
+    public void  setPrice(double price){
+        this.price= price;
+    }
+
     public static void  main(String[] args){
-        Book myBook1=new Book("The Pearl","Coyotito",2000.0);
-        System.out.println("the book tittle is" +" " + myBook1.tittle +" " +"by " + myBook1.author+" " +"at ksh" +myBook1.price); 
+        Book myBook1= new Book("The Pearl","Coyotito",500);
+        myBook1.setTittle("The Pearl");
+        myBook1.setAuthor("Coyotito");
+        myBook1.setPrice(600);
+        System.out.println("the book tittle is" +" " + myBook1.getTittle() +" " +"by " + myBook1.getAuthor()+" " +"at ksh" +myBook1.getPrice()); 
     }
 
 }
---
+++ OOPS/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle{
+    protected String brand;
+    protected double speed;
+    public Vehicle(String brand,double speed){
+        this.brand=brand;
+        this.speed=speed;
+    }
+    
+}
